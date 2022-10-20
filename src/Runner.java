371d9e9a71c58ccac2c43a8161d103d9dc513227
James Correia <jcorr851@gmail.com>
2022-10-19 22:22:51 -0400

    Changes from home wed

--- src/Runner.java
+++ src/Runner.java
@@ -6,16 +6,18 @@ import java.util.*;
 import java.util.function.BiConsumer;
 
 public class Runner {
-    public static String DIR = "/cs/home/jcorr851/Documents/netflix.data";
+    public static String DIR = "C:\\Users\\xxmem\\Desktop\\school\\4\\Big Data Systems\\A1\\netflix.data";
     public static double SUPPORT_THRESHOLD = 0.01;
     public static double threshold; // As a number of items, not a fraction.
     public static Integer num_baskets;
     public static long end_time = 0;
+    public static int feedback_interval = 5000; // On pass one, how many lines should be parsed before another status update is given
 
     public static void main(String[] args) throws IOException {
         final long startTime = System.currentTimeMillis();
         //Do the first pass, get back a tuple of the counts HashMap, and the number of baskets.
         Tuple<HashMap<Integer, Integer>, Integer> counts = get_item_counts();
+        System.out.println("Got item counts.");//Debug
 
         // Move out of tuple into variables
         HashMap<Integer, Integer> map = counts.get_left();
@@ -23,17 +25,22 @@ public class Runner {
 
         threshold = num_baskets * SUPPORT_THRESHOLD; // number of occurances to be considered frequent.
 
+        System.out.println("Calculated threshold.");//Debug
 
         //Trim the hashmap to exclude items below threshold.
         map.values().removeIf(value -> value < threshold);
 
+        System.out.println("Trimmed hashmap.");//Debug
+
         //Perform Second Pass, find frequent itemsets
         HashMap<HashSet<Integer>, Integer> ans = find_frequent_pairs(map);
 
+        System.out.println("Found frequent pairs"); //Debug
+
         System.out.println(ans.toString());
 
-        final long endTime = System.currentTimeMillis();
-        System.out.println("Execution time: " + (endTime - startTime)/1000.0 + "s");
+        end_time = System.currentTimeMillis();
+        System.out.println("Execution time: " + (end_time - startTime)/1000.0 + "s");
 
     }
 
@@ -44,6 +51,7 @@ public class Runner {
         int num_rows = 0;
         try {
             reader = new BufferedReader(new FileReader(DIR));
+            int baskets_parsed = 0; //For statistics
             String basket;
             basket = reader.readLine();
             while (basket != null) {
@@ -57,6 +65,14 @@ public class Runner {
                         counts.put(i, (Integer) counts.get(i) + 1); //Increment count by 1.
                     }
                 }
+
+
+                // Progress indicator
+                baskets_parsed++;
+                if ((baskets_parsed % (feedback_interval) == 0)) {
+                    System.out.println("counted basket #" + baskets_parsed);
+                }
+
                 basket = reader.readLine();
             }
 
@@ -104,7 +120,7 @@ public class Runner {
                 for (int i = 0; i <= ints.size() - 2; i++){
                     if (counts.containsKey(ints.get(i))) { //If the element was frequent
                         for (int j = i + 1; j <= ints.size() - 1; j++){
-                            if (counts.containsKey(ints.get(i))){
+                            if (counts.containsKey(ints.get(j))){
                                 HashSet<Integer> pair = new HashSet<>();
                                 pair.add(ints.get(i));
                                 pair.add(ints.get(j));
--- src/Tuple.java
+++ src/Tuple.java
@@ -14,4 +14,25 @@ public class Tuple<L, R> {
     public R get_right(){
         return right;
     }
+
+    public boolean equals(Object o){
+        if (o.getClass() != this.getClass()){
+            return false;
+        }
+
+        Tuple<?, ?> p = (Tuple<?,?>) o;
+        return (this.left == p.get_left() && this.right == p.get_right());
+    }
+
+    public int hashCode() {
+        if (this.left == null || this.right == null){
+            return 0;
+        }
+
+        int ret = 991;
+        ret = 433 * ret + left.hashCode();
+        ret = 433 * ret + right.hashCode();
+        return ret;
+    }
 }
+
